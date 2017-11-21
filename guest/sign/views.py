8c59ba254943aa7a5c3ace58da03263389d66709
ArsenalLevel3 <18751967189@163.com>
2017-11-21 18:07:15 +0800

    second push

--- guest/sign/views.py
+++ guest/sign/views.py
@@ -3,10 +3,19 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
-# from django.http import HttpResponse
+from django.http import HttpResponse
 
 
 # Create your views here.
 def index(request):
     return render(request, "index.html")
 
+
+def login_action(request):
+    if request.method == 'POST':
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+        if username == 'admin' and password == 'admin123':
+            return HttpResponse('login successfully!!')
+        else:
+            return render(request, 'index.html', {'error': 'username or password error!!'})
